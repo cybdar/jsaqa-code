695a88c360894e7428cae1e29c99697a2fa4c368
cybdar <kari4ka21@gmail.com>
2026-04-08 03:04:39 +0200

    add Cypress E2E tests

--- 7.7/cypress/support/e2e.js
+++ 7.7/cypress/support/e2e.js
@@ -2,6 +2,9 @@
 // This example support/e2e.js is processed and
 // loaded automatically before your test files.
 //
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
 // You can change the location of this file or turn off
 // automatically serving support files with the
 // 'supportFile' configuration option.
@@ -15,22 +18,3 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-// Игнорируем известные ошибки приложения
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // Список ошибок приложения, которые мы хотим игнорировать
-  const appErrors = [
-    'startSales is not defined',
-    'chairChecked is not defined'
-  ];
-  
-  // Проверяем, содержит ли сообщение об ошибке одну из известных
-  const isAppError = appErrors.some(errorMsg => err.message.includes(errorMsg));
-  
-  if (isAppError) {
-    // Возвращаем false, чтобы Cypress НЕ провалил тест из-за этой ошибки
-    return false;
-  }
-  // Для всех остальных ошибок возвращаем true, чтобы тест упал
-  return true;
-})
